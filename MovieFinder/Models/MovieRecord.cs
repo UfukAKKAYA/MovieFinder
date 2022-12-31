70d9aea041bd4c59516576f8d8c519d71239ca3f
Ufuk AKKAYA <akkaya_ufuk@hotmail.com>
2023-01-01 02:21:09 +0300

    --> Eksik dosyalar yüklendi.

---
+++ MovieFinder/Models/MovieRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFinder.Models
+{
+    public class MovieRecord
+    {
+        public Movie Movie { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
